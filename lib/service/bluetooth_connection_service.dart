017caef926d54641b1dbaaafc7151b03dcf78eee
alex.tifox <tilnyi.olexander@gmail.com>
2020-11-16 16:36:08 +0100

    Bluetooth connection service interface

--- lib/service/bluetooth_connection_service.dart
+++ lib/service/bluetooth_connection_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
-import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:logger/logger.dart';
 
 class BluetoothConnectionService {
   static final BluetoothConnectionService _instance =
@@ -11,25 +12,69 @@ class BluetoothConnectionService {
   }
 
   BluetoothConnectionService._internal() {
-    _bluetoothStreamController = StreamController();
+    _serviceToConnectionStreamController = StreamController();
+    _serviceToConnectionStreamOut = _serviceToConnectionStreamController.stream;
+    serviceToConnectionStreamIn = _serviceToConnectionStreamController.sink;
+
+    _connectionToServiceStreamController = StreamController();
+    _connectionToServiceStreamIn = _connectionToServiceStreamController.sink;
+    connectionToServiceStreamOut = _connectionToServiceStreamController.stream;
   }
 
-  FlutterBlue _flutterBlue = FlutterBlue.instance;
+  /// Represents bluetooth connection to the device
+  BluetoothConnection _connection;
 
-  StreamController _bluetoothStreamController;
+  /// Shows bluetooth state of a physical device (smartphone)
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
 
-  void startScanning() {
-    _flutterBlue.startScan(timeout: Duration(seconds: 4));
+  /// Instance of a [FlutterBluetoothSerial] which implements bridge between
+  /// application and native implemented BT workflow
+  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
 
-    var subscription = _flutterBlue.scanResults.listen((results) {
-      // do something with scan results
-      for (ScanResult r in results) {
-        print('${r.device.name} found! rssi: ${r.rssi}');
-      }
-    });
+  /// Current device you are connected to
+  BluetoothDevice _device;
+  Logger _logger = Logger();
 
-    _flutterBlue.stopScan();
+  /// Stream controller intended for one-way 'in' communication between any service and
+  /// this [BluetoothConnectionService] via service facts i.e. data requests
+  StreamController _serviceToConnectionStreamController;
 
-    subscription.cancel();
-  }
+  /// StreamSink designed for exposing ability of sending requests to the
+  /// bluetooth connection service
+  StreamSink serviceToConnectionStreamIn;
+
+  /// This stream purpose is to listen for requests came outside
+  /// [BluetoothConnectionService] service
+  Stream _serviceToConnectionStreamOut;
+
+  /// Stream controller intended for one-way 'out' communication between
+  /// [BluetoothConnectionService] and any other service, which can connect to
+  /// this service
+  StreamController _connectionToServiceStreamController;
+
+  /// Stream sink designed for sending responses need to be handled for any
+  /// service connected to this [BluetoothConnectionService]
+  StreamSink _connectionToServiceStreamIn;
+
+  /// This stream purpose is to expose ability of listening responses from
+  /// [_connectionToServiceStreamController] in [BluetoothConnectionService]
+  Stream connectionToServiceStreamOut;
+
+  /// When user choose a device he wants to connect this method stores the
+  /// device instance user is connected to
+  void setBluetoothDevice() {}
+
+  void connectToDevice() {}
+  void disconnectFromDevice() {}
+
+  void enableBluetooth() {}
+
+  void getPairedDevices() {}
+
+  /// Method helps to send data to the device via [_connection] user
+  /// is connected to
+  void sendData() {}
+
+  /// Listener function designed to handle data came from [_connection]
+  void receiveData() {}
 }
