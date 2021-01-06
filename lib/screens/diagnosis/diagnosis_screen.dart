b08ac6c99f18b815207bbb3e789e70c8d2f51196
alex.tifox <tilnyi.olexander@gmail.com>
2021-01-06 22:31:03 +0100

    Styled Dtc codes list tiles

--- lib/screens/diagnosis/diagnosis_screen.dart
+++ lib/screens/diagnosis/diagnosis_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../screens/diagnosis/widgets/dtc_code_expansion_tile.dart';
+import '../../blocs/blocs.dart';
 import '../../widgets/custom_elevated_button.dart';
 import '../../blocs/dtc/dtc_bloc.dart';
 
@@ -33,25 +35,31 @@ class DiagnosisScreen extends StatelessWidget {
                     child: ListView(
                       shrinkWrap: true,
                       children: [
-                        ...dtcState.dtcCodesList.map(
-                          (dtc) => ExpansionTile(
-                            title: Text(dtc.dtcShortName),
-                          ),
-                        ),
+                        ...dtcState.dtcCodesList
+                            .map((dtc) => DtcCodeExpansionTile(dtcCode: dtc)),
                       ],
                     ),
                   ),
                 ],
               );
             } else {
-              return Column(
-                children: [
-                  Text('You can start scanning for issues'),
-                  CustomElevatedButton(
-                    onPressed: _startScanForDtc,
-                    buttonText: 'START',
-                  ),
-                ],
+              return Container(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Text(
+                      'You can start scanning for issues',
+                      style:
+                          Theme.of(context).primaryTextTheme.headline6.copyWith(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                    ),
+                    CustomElevatedButton(
+                      onPressed: _startScanForDtc,
+                      buttonText: 'START',
+                    ),
+                  ],
+                ),
               );
             }
           },
