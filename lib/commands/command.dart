cd475269d2acae95d3c6d10a1a804563e1e29a5e
alex.tifox <tilnyi.olexander@gmail.com>
2021-01-07 22:09:02 +0100

    Refactoring ELM command mechanism and initial device configuration

--- lib/commands/command.dart
+++ lib/commands/command.dart
@@ -1,23 +1,12 @@
 import 'dart:typed_data';
 
-import 'package:flutter/material.dart';
-
-import '../service/bluetooth_data_helper.dart';
-
 /// Base of any command sent via bluetooth
 abstract class Command {
-  Uint8List _rawDataForSending;
-
-  Command({
-    @required String command,
-  }) {
-    _rawDataForSending =
-        BluetoothDataHelper.transformCommandToSendReadyFormat(command);
-  }
-
   /// This getter is mandatory if you want to sent data to bluetooth connection
   ///
   /// This getter is responsible for data which is stored in Uint8List format
   /// which is required by [BluetoothConnection]
-  Uint8List get dataToSend => _rawDataForSending;
+  Uint8List getDataToSend();
+
+  Uint8List _constructDataToSend();
 }
--- lib/commands/elm_command.dart
+++ lib/commands/elm_command.dart
@@ -1,5 +1,63 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import '../commands/command.dart';
 
+enum ElmCommands { reset, echo, spaces, protocol, lines }
+
+enum ElmOptionState { ON, OFF, AUTO }
+
 class ElmCommand extends Command {
-  ElmCommand(String command) : super(command: command);
+  static const String _atCommand = 'AT';
+  final ElmCommands elmCommand;
+  final ElmOptionState optionState;
+  final bool isInitialConfiguration;
+
+  ElmCommand({
+    this.elmCommand,
+    this.optionState,
+    this.isInitialConfiguration,
+  }) : assert(
+            (elmCommand != null && optionState != null) ||
+                (elmCommand == null &&
+                    optionState == null &&
+                    isInitialConfiguration != null),
+            'If you want to make initial configuration using one command - '
+            'provide only required isInitialConfiguration, otherwise '
+            'provide both command and option state');
+
+  static const Map<ElmCommands, Map<ElmOptionState, String>>
+      _availableElmCommands = {
+    ElmCommands.reset: {
+      ElmOptionState.AUTO: 'Z',
+    },
+    ElmCommands.echo: {
+      ElmOptionState.ON: 'E1',
+      ElmOptionState.OFF: 'E0',
+      ElmOptionState.AUTO: 'E0',
+    },
+    ElmCommands.spaces: {
+      ElmOptionState.ON: 'S1',
+      ElmOptionState.OFF: 'S0',
+      ElmOptionState.AUTO: 'S0',
+    },
+    ElmCommands.protocol: {
+      ElmOptionState.AUTO: 'SP 0',
+    },
+    ElmCommands.lines: {
+      ElmOptionState.ON: 'L1',
+      ElmOptionState.OFF: 'L0',
+      ElmOptionState.AUTO: 'L0',
+    },
+  };
+
+  @override
+  Uint8List _constructDataToSend() => utf8.encode(_atCommand +
+      ElmCommand._availableElmCommands[this.elmCommand][this.optionState]);
+
+  @override
+  Uint8List getDataToSend() {
+    // TODO: implement getDataToSend
+    throw UnimplementedError();
+  }
 }
